9a1334030be1e5dc539e544ee3bb2bb5528d3b94
RyanCheng98153 <ryan98153@gmail.com>
2024-08-24 00:49:25 +0800

    feat: 7*7 empty grid 2d graph regular node

--- maple-leaf/main.py
+++ maple-leaf/main.py
@@ -1,6 +1,8 @@
 import sys
 from src.graph import MLGraph
 from src.helper import PrintHelper, NodeHelper
+import networkx as nx
+import matplotlib.pyplot as plt
     
 def main():
     L:int = int(sys.argv[1])
@@ -12,10 +14,43 @@ def main():
     
     graph = MLGraph(L, W, hexInit)
     graph.makeGraph()
-    graph.printGraphText()
+    # graph.printGraphText()
     # graph.printGraphVisual()
     # checkIsingleNode(graph)
+    visualize(graph)
     
+def visualize( _graph: MLGraph ):
+    adjList = []
+    
+    for srcNode in _graph.nodes:
+        if srcNode == None:
+            continue
+        for adjNode in [srcNode.right, srcNode.bottom, srcNode.bottomRight]:
+            if adjNode == None:
+                continue
+            adjList.append([srcNode.id, adjNode.id])
+
+    # G = nx.DiGraph()
+    # G.add_edges_from(adjList)
+    # G = nx.grid_2d_graph(_graph.L, _graph.W)
+    # pos = dict( (n, n) for n in G.nodes() ) #Dictionary of all positions
+    # labels = dict( ((i, j), i + (_graph.L-1-j) * _graph.W ) for i, j in G.nodes() )
+    # nx.draw_networkx(G, pos=pos, labels=labels,with_labels=False, node_size=10)
+    N=7
+    G=nx.grid_2d_graph(N,N)
+    pos = dict( (n, n) for n in G.nodes() ) #Dictionary of all positions
+    labels = dict( ((i, j), i + (N-1-j) * N ) for i, j in G.nodes() )
+    print(f"G: {G}")
+    print(f"pos: {pos}")
+    print(f"labels: {labels}")
+    nx.draw_networkx(G, pos=pos, labels=labels,with_labels=True, node_size=10)
+    
+    
+    # nx.draw_networkx(G) 
+    plt.show() 
+    # print(adjList)
+    return adjList
+
 def checkIsingleNode( _graph: MLGraph ):
     helper = NodeHelper(_graph.L, _graph.W)
     n = 0
