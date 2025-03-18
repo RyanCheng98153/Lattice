4a4d81ad075731190c039ba5d96c12c1dabe7d80
ryan98153 <110703007@nccu.edu.tw>
2025-03-18 18:23:10 +0800

    refactor: triangular with displayType

--- triangular/main.py
+++ triangular/main.py
@@ -1,66 +1,64 @@
 import sys
 from src.graph import TrianglularGraph
-from src.visualize import Visualize
+from src.visualize import Visualize, DisplayType
 from src.node import Spin
 import argparse
+
+def getFileQubos(inputFile: str):
+    with open(inputFile, "r") as f:
+        datas = [line for line in f.readlines() if not line.startswith("#")]
+    
+    ids = [int(id) for id in datas[0].split()[:-2]]
+    
+    results = []
+    for data in datas[1:]:
+        if data.startswith("['SPIN'"):
+            break
+        if not data[0].isdigit():
+            continue
+        
+        items = data.split()
+        qubos = [ -1 if var == "-1" else 1 for var in items[1:-2] ]
+        energy = items[-2]
+        
+        if energy.startswith("-"):
+            energy = -1 * float(energy[1:])
+        else:
+            energy = float(energy)
+        
+        results.append( { "qubos": qubos, "energy": energy })
+        
+    return results
 
 def main(args: argparse.Namespace):
     L:int = 6
     W:int = 6
     
-    # L:int = int(sys.argv[1])
-    # W:int = int(sys.argv[2])
     L:int = args.L
     W:int = args.W
-    
-    hexInit:int = 0
-    # if (len(sys.argv) > 3):
-    #     hexInit = int(sys.argv[3])
     
     graph = TrianglularGraph(L, W)
     graph.makeGraph()
     graph.bondGraph(1.0)
     
-    # if len(sys.argv) == 4:
     if args.inputFile is not None:
-        # with open(sys.argv[3], "r") as f:
-        with open(args.inputFile, "r") as f:
-            datas = [line for line in f.readlines() if not line.startswith("#")]
+        results = getFileQubos(args.inputFile)
         
-        ids = [int(id) for id in datas[0].split()[:-2]]
-        
-        results = []
-        for data in datas[1:]:
-            if data.startswith("['SPIN'"):
-                break
-            if not data[0].isdigit():
-                continue
-            
-            items = data.split()
-            qubos = [ -1 if var == "-1" else 1 for var in items[1:-2] ]
-            energy = items[-2]
-            
-            if energy.startswith("-"):
-                energy = -1 * float(energy[1:])
-            else:
-                energy = float(energy)
-            
-            results.append( { "qubos": qubos, "energy": energy })
-        
-        i = 0
-        
-        qubos = results[i]["qubos"]
+        qubos = results[0]["qubos"]
         
         for node in graph.nodes:
             if node is not None:
                 spin = Spin.UP if qubos[node.clean_id] == 1 else Spin.DOWN
                 node.spin = spin
-            # print((node.clean_id, node.spin) if node is not None else None)
-        Visualize.visualize(graph, showStrength=False, fromfile=True, save_fig=args.saveFig)
-        pass
-    else:
-        Visualize.visualize(graph, showStrength=False, save_fig=args.saveFig)
-        pass
+
+    # check if the args.display_type match DisplayType's value
+    display_type = {
+        "plain": DisplayType.PLAIN,
+        "spinfile": DisplayType.SPIN_FILE,
+        "orderP": DisplayType.ORDER_PARAMETERS
+    }.get(args.displayType)
+    
+    Visualize.visualize(graph, labelHexagon=False, showStrength=False, save_fig=args.saveFig, display_type=display_type)
     
     if args.saveLattice:
         with open(file=f"./triangular_L_{L}_{W}.txt", mode="w") as f:
@@ -73,6 +71,13 @@ if __name__ == "__main__":
     parser.add_argument("--inputFile", required=False, type=str, help="file path")
     parser.add_argument("--saveFig", required=False, default=False , action="store_true", help="save figure")
     parser.add_argument("--saveLattice", required=False, default=False , action="store_true", help="save lattice file")
+    parser.add_argument("--displayType", required=False, default=None, type=str, help="display type (plain, spinfile, orderP)")
     args = parser.parse_args()
     
+    if args.displayType is None:
+        if args.inputFile is None:
+            args.displayType = "plain"
+        else:
+            args.displayType = "spinfile"
+        
     main(args)
--- triangular/src/visualize.py
+++ triangular/src/visualize.py
@@ -2,15 +2,33 @@ import networkx as nx
 from copy import deepcopy
 import matplotlib.pyplot as plt
 from src.graph import TrianglularGraph
-from src.node import BondType, NodeType, Spin
+from src.node import BondType, Spin
 from math import sqrt
- 
+from enum import Enum
+
+class DisplayType(Enum):
+    PLAIN = "plain"
+    SPIN_FILE = "spinfile"
+    ORDER_PARAMETERS = "orderP"
+    
+    def description(self):
+        descriptions = {
+            self.PLAIN: "Displaying theorical graph with no styling",
+            self.SPIN_FILE: "Displaying spin of input file",
+            self.ORDER_PARAMETERS: "Displaying order parameters",
+        }
+        return descriptions[self]
+
 class Visualize:
     def __init__(self) -> None:
         pass
     
     @staticmethod
-    def visualize( _graph: TrianglularGraph, showStrength=False, fromfile=False, save_fig=False ):
+    def visualize(
+        _graph: TrianglularGraph, 
+        labelHexagon = False, showStrength=False, save_fig=False, 
+        display_type: DisplayType = DisplayType.PLAIN
+    ):
         G: nx.Graph = nx.empty_graph( n=0 )
         
         def getPosition (_id):
@@ -18,58 +36,72 @@ class Visualize:
             y = _graph.L-1-y
             return (x, y)
         
-        def getLightColor (color):
-            if color == "Red" or color == "red":
-                return "pink"
-            return "light" + color
-        
         def getSpinColor(spin: Spin):
             if spin == Spin.UP:
                 return "lightblue"
             if spin == Spin.DOWN:
                 return "blue"
         
-        # ignore the hexagon None ndoes
-        G.add_nodes_from( getPosition(node.id) for node in _graph.nodes if node is not None )
-        if fromfile:
-            color_map = [getSpinColor(node.spin) for node in _graph.nodes if node is not None]
+        def getOrderColor(id: int) -> str:
+            i, j = _graph.helper.getCood(id)
+            
+            return {
+                0: "red",
+                1: "blue",
+                2: "lightgreen"
+            }.get(( i+j+1 ) % 3)
+        
+        # add Graphs nodes to networkx graph
+        if labelHexagon:
+            G.add_nodes_from(getPosition(id) for id in range(len(_graph.nodes)))
+            nodes = _graph.nodes
         else:
-            color_map = ["lightgray" for node in _graph.nodes if node is not None]
-            # print(len(color_map))
-            # use spin color
-            # color_map = [(node.NodeType.name) for node in _graph.nodes if node is not None]
-
+            nodes = [node for node in _graph.nodes if labelHexagon or node is not None]
+            G.add_nodes_from(getPosition(node.id) for node in nodes)
+        
+        if display_type == DisplayType.PLAIN:
+            color_map = ["lightgray" if node is not None else "grey" for node in nodes]
+        elif display_type == DisplayType.SPIN_FILE:
+            color_map = [getSpinColor(node.spin) if node is not None else "grey" for node in nodes]
+        elif display_type == DisplayType.ORDER_PARAMETERS:
+            color_map = [getOrderColor(node.id) if node is not None else "grey" for node in nodes]
+        else:
+            raise ValueError("Invalid display type")
+        
         nodelist = deepcopy(G.nodes()) # using deep copy not shallow copy because nx.network may return reference
         
-        def getBondStyle(bondType: BondType) -> tuple[str, str]:
-            return "black", "-"
+        def getBondStyle(bondType: BondType, no_color = True) -> tuple[str, str]:
+            if no_color:
+                return "gray", "-"
+            if bondType == BondType.Hexagon:
+                return "blue", "-"
+            if bondType == BondType.Triangle:
+                # return "orange", ":"
+                return "orange", "--"
+            if bondType == BondType.Dimer:
+                return "red", "-"
                     
         for srcId, adjId, bondStrength, bondType in _graph.getAdjList():
-            if bondType == None:
+            if bondType == None or bondType == BondType.Connected:
                 bondColor, bondStyle = "black", "-"
             else:
                 bondColor, bondStyle = getBondStyle(bondType)
-                
-            srcX, srcY = getPosition(srcId)
             
-            # right bottom periodic
+            # right button periodic
             if srcId == len(_graph.nodes) -1 and adjId == 0:
-                G.add_edge( (srcX, srcY) , (_graph.W, -1), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
-                
+                G.add_edge(getPosition(srcId), (_graph.W, -1), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
                 continue
             # right periodic column
             if (srcId+1) % _graph.W == 0 and adjId % _graph.W == 0:
                 adjX, adjY = getPosition(adjId)
-                G.add_edge( (srcX, srcY), (_graph.W, adjY), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
-                
+                G.add_edge(getPosition(srcId), (_graph.W, adjY), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
                 continue
             # bottom periodic row
             if srcId // _graph.W == _graph.L-1 and adjId // _graph.W == 0:
                 adjX, adjY = getPosition(adjId)
-                G.add_edge( (srcX, srcY), (adjX, -1), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
-                
+                G.add_edge(getPosition(srcId), (adjX, -1), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
                 continue
-            G.add_edge( (srcX, srcY), getPosition(adjId), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
+            G.add_edge(getPosition(srcId), getPosition(adjId), weight=bondStrength, bondColor=bondColor, bondStyle=bondStyle)
         
         sqrt3 = sqrt(3)
         pos = dict( ((i, j), (i+j/2, j*sqrt3)) for (i, j) in G.nodes() ) #Dictionary of all positions
@@ -78,19 +110,29 @@ class Visualize:
         # draw
         bondColor = nx.get_edge_attributes(G, 'bondColor').values()
         bondStyle = nx.get_edge_attributes(G, 'bondStyle').values()
-        # nodeColor = nx.get_edge_attributes(G, "nodeColor").values()
         weight_labels = nx.get_edge_attributes(G,'weight')
         
         node_width = 250 if _graph.L == 7 else 200
         edge_width = 3 if _graph.L == 7 else 2
         font_size = 10 if _graph.L == 7 else 8
         
-        color_map.extend(["gray" for _ in range(G.nodes.__len__() - len(color_map))])
+        # small node with no font size
+        node_width = 100
+        show_labels = False
         
-        fontcolor = "black" if fromfile else "black"
+        # color of periodic nodes
+        node_num = len(color_map)
+        for i in range(G.nodes.__len__() - node_num):
+            
+            if DisplayType.SPIN_FILE:
+                color_map.append("gray")
+            elif DisplayType.ORDER_PARAMETERS:
+                color_map.append("gray")
+            elif DisplayType.PLAIN:
+                color_map.append("orange")
         
-        nx.draw(G, pos, labels=labels, with_labels=True, 
-                node_size=node_width, font_size=font_size, font_color=fontcolor,
+        nx.draw(G, pos, labels=labels, with_labels=show_labels, 
+                node_size=node_width, font_size=font_size,
                 node_color = color_map, edgecolors="black",
                 edge_color=bondColor, width=edge_width, style=bondStyle)    
         # add label of bond edge strength
@@ -99,5 +141,5 @@ class Visualize:
         
         if save_fig:
             plt.savefig(f"./fig_triangular_{_graph.L}_{_graph.L}.pdf", format='pdf', bbox_inches='tight')
-        else:
-            plt.show()
+        # else:
+            # plt.show()
